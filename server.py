32ccec407a93c03a51ba09decfb9d4e6dee540cf
Paul Larsen <pn1315@ic.ac.uk>
2017-05-23 17:25:56 +0100

    partial revert of authentication removal - app.config['SECRET_KEY'] should not be removed.

--- server.py
+++ server.py
@@ -9,6 +9,7 @@ from flask_socketio import SocketIO, Namespace, emit, join_room, leave_room, \
 async_mode = None
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode=async_mode)
 thread = None
 
