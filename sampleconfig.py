33a0e33926951245f1543985ebd6a1c7871cb5bc
Paul Larsen <pn1315@ic.ac.uk>
2017-05-29 16:30:30 +0100

    change config slightly to use caps names for the PSQL parts

--- sampleconfig.py
+++ sampleconfig.py
@@ -5,8 +5,9 @@ class Development(object):
     # Change below otherwise it will obviously not work.
     JS_UPLOAD_FOLDER = '/path/to/dir/PhoneMapServer/data/js/'
     ZIP_UPLOAD_FOLDER = '/path/to/dir/PhoneMapServer/data/zip/'
-    PostGreSQLUser = 'I_am_a_user'
-    PostGreSQLPassword = 'this_is_a_password'
-    PostGreSQLDB = 'this_is_a_DB'
-    PostGreSQLServer = 'db.this.is.where.com'
-    PostGreSQLPort = 6666
+
+    PSQL_USER = 'I_am_a_user'
+    PSQL_PASSWORD = 'this_is_a_password'
+    PSQL_DB = 'this_is_a_DB'
+    PSQL_SERVER = 'db.this.is.where.com'
+    PSQL_PORT = 6666
