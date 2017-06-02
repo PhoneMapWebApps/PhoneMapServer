3dd3676cec6d1f7ba6e0d053f02ab4f8cefbf72c
Rob <robmoore121@gmail.com>
2017-06-02 02:07:17 +0100

    Fixing logs so it works on CI

--- misc/logger.py
+++ misc/logger.py
@@ -1,6 +1,13 @@
-log_file = open("logs/log.txt", "a+")
+import os
+
+logs_dir = 'logs'
+
+if not os.path.exists('logs'):
+    os.makedirs('logs')
+
+log_file = open('logs/log.txt', 'a+')
 
 
 def log(s):
-    log_file.write(s + "\n")
+    log_file.write(s + '\n')
     log_file.flush()
