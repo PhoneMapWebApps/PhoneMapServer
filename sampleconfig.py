a903a09e1799a8de18c67490cee76614c216c5eb
Paul Larsen <pn1315@ic.ac.uk>
2017-06-03 14:15:34 +0100

    more config changes for circleci to play nice

--- sampleconfig.py
+++ sampleconfig.py
@@ -1,18 +1,4 @@
 # sample config -> copy paste to a new config.py which isn't under VCS (has been added to .gitignore already)
-import os
-
-class Development(object):
-    DEBUG = True
-    SECRET_KEY = 'secret!'
-    # Change below otherwise it will obviously not work.
-    JS_UPLOAD_FOLDER = '/path/to/dir/PhoneMapServer/data/js/'
-    ZIP_UPLOAD_FOLDER = '/path/to/dir/PhoneMapServer/data/zip/'
-
-    # SQLALCHEMY_DATABASE_URI = postgresql://[user[:password]@][netloc][:port][/dbname][?param1=value1&...]
-    SQLALCHEMY_DATABASE_URI = 'sqltype://username:password@location:port/databasename'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
 class Config(object):
     DEBUG = False
     TESTING = False
@@ -40,4 +26,4 @@ class TestingConfig(Config):
     JS_UPLOAD_FOLDER = "test/upload/data/js/"
     ZIP_UPLOAD_FOLDER = "test/upload/data/zip/"
     # setup to work with circleCI
-    SQLALCHEMY_DATABASE_URI = 'postgresql://ubuntu/circle_test'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://ubuntu@localhost:5432/circle_test'
