0ada3ac76f1a8be4efa8d8f739965b697e5a4be5
Rob <robmoore121@gmail.com>
2017-06-04 00:14:18 +0100

    additional check for add_to_db test

--- test/test_sql_db.py
+++ test/test_sql_db.py
@@ -1,4 +1,5 @@
 import unittest
+from datetime import datetime
 
 from app import create_app, app, db
 from app.main import sql
@@ -10,16 +11,24 @@ class TestSQLdb(unittest.TestCase):
     def setUp(self):
         create_app(False, True)
 
+    @classmethod
+    def millis_within_range(self, t1, t2, max_seperation):
+        return (0.001 * abs(t1 - t2).microseconds) < max_seperation
+
     def test_add_to_db(self):
         with app.app_context():
             js_file = open('test/resources/test.js', 'rb')
             zip_file = open('test/resources/test.zip', 'rb')
 
             old_nTasks = SubTasks.query.count()
+            submission_time = datetime.utcnow()
             add_to_db(js_file, zip_file)
             new_nTasks = SubTasks.query.count()
 
             self.assertEqual(new_nTasks, old_nTasks + 1)
+
+            subtask = SubTasks.query.one()
+            assert self.millis_within_range(subtask.time_submitted, submission_time, 20.0)
 
     def test_get_phone_in_db(self):
         with app.app_context():
